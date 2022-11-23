52edc07e9a0c782e17622b64bec61280dec226d1
Fixckpx <64030012@kmitl.ac.th>
2022-11-23 15:46:42 +0700

    add Dev1

--- HelloWorld/HelloWorld/Form1.Designer.cs
+++ HelloWorld/HelloWorld/Form1.Designer.cs
@@ -29,23 +29,36 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
-            this.label1.Location = new System.Drawing.Point(293, 154);
+            this.label1.Location = new System.Drawing.Point(281, 71);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(224, 95);
+            this.label1.Size = new System.Drawing.Size(237, 95);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Main";
+            this.label1.Text = "Dev1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(297, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 102);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Push Me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +70,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
